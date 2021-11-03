15e1f405a770af9b8d7f9f642a3bef0d5d6663c8
Batuhan <bipci@myseneca.ca>
2021-11-03 18:18:24 +0300

    assign4 update- hbs files added

--- scripts/login-form.js
+++ scripts/login-form.js
@@ -1,49 +1,32 @@
+const form = document.querySelector('.login_form');
 
-    const form = document.querySelector('.login_form');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
 
-    const email = document.getElementById('mail');
-    const password = document.getElementById('password');
+const emailError = document.querySelector('#email + span.error');
+const passwordError = document.querySelector('#password + span.error');
 
-    const emailError = document.querySelector('#mail + span.error');
-    const passwordError = document.querySelector('#password + span.error');
-
-
-    email.addEventListener('input', (email, password) => {
-      if (email.validity.valid && password.validity.valid) {
-        emailError.innerHTML = '';
-        emailError.className = 'error';
-
-        passwordError.innerHTML = '';
-        passwordError.className = 'error';
-      } else showError();
-    });    
-
-    form.addEventListener('submit', (event) => {
-      if (!email.validity.valid) {
-        showError();
-        event.preventDefault();
-      } else if (!password.validity.valid) {
-        showError();
-        event.preventDefault();
-      }
-    });
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  showError();
+});
 
 
-    showError = () => {
-      if (email.validity.valueMissing && password.validity.valueMissing) {
-        emailError.textContent = 'Please enter e-mail address.';
-        passwordError.textContent = 'Forgot your password ?';
-        emailError.className = 'error active';
-        passwordError.className = 'error active';
-      } else if (email.validity.typeMismatch) {
-        emailError.textContent = 'Entered value must be an e-mail address.';
-        emailError.className = 'error active';
-      } else if (email.validity.valueMissing) {
-        emailError.textContent = 'Enter e-mail address.';
-        emailError.className = 'error active';
-      } else if (password.validity.valueMissing) {
-        passwordError.textContent = 'Forgot your password ?';
-        passwordError.className = 'error active';
-      }
 
-    }
+showError = () => {
+  if (email.validity.valueMissing && password.validity.valueMissing) {
+    emailError.textContent = 'Please enter e-mail address.';
+    passwordError.textContent = 'Forgot your password ?';
+    emailError.className = 'error active';
+    passwordError.className = 'error active';
+  } else if (email.validity.typeMismatch) {
+    emailError.textContent = 'Entered value must be an e-mail address.';
+    emailError.className = 'error active';
+  } else if (email.validity.valueMissing) {
+    emailError.textContent = 'Enter e-mail address.';
+    emailError.className = 'error active';
+  } else if (password.validity.valueMissing) {
+    passwordError.textContent = 'Forgot your password ?';
+    passwordError.className = 'error active';
+  }
+} 
--- server.js
+++ server.js
@@ -1,53 +1,107 @@
 const express = require("express");
-const app = express();
-const HTTP_PORT = process.env.PORT || 8080;
-const path = require("path");
+const server = express();
+const exphbs = require("express-handlebars");
+const {check,validationResult} = require("express-validator");
+const {
+  appendFile
+} = require("fs");
 
-const {check, validationResult} = require("express-validator");
-const { json } = require("body-parser");
-app.use(express.json())
-app.use(express.urlencoded({ extended: true })); //Middleware _aka:bodyparser
 
-app.use("/static", express.static(__dirname + "/static"));
-app.use("/scripts", express.static(__dirname + '/scripts'));
+//parse incoming req aka middleware
+server.use(express.urlencoded({extended: false}));
+server.use(express.json());
 
-onHttpStart = () => {
-  console.log("server listening on: " + HTTP_PORT);
-};
+//Static Files
+server.use("/static", express.static(`${__dirname}/static`));
+server.use("/scripts", express.static(`${__dirname}/scripts`));
 
+//Templating Engine
+server.set('view engine', ".hbs");
+server.engine(".hbs", exphbs({
+  extname: ".hbs",
+  defaultLayout: "main"
+}));
 //home
-app.get("/", (req, res) => {
-  res.status(200).sendFile(path.join(__dirname, "/views/home.html"));
+server.get("/", (req, res) => {
+  res.render("home", {
+    title: "home"
+  })
 });
-
 //Meals Package
-app.get("/mealPack", (req, res) => {
-  res.status(200).sendFile(path.join(__dirname, "/views/mealPack.html"));
+server.get("/mealPack", (req, res) => {
+  res.render("mealPack", {
+    title: "mealPack"
+  })
+});
+//user forms aka login
+server.get("/userForms", (req, res) => {
+  res.render("userForms", {
+    title: "userForms",
+    layout: "register"
+  })
 });
 
-//todo login page & register
-/* app.post("/userForms", (req , res) => {
-  const userFormData = req.body;
-  console.log(res.send(userFormData));
-  console.log(json.stringify(userFormData));
-}) */
-app.get("/userForms", (req , res, next) => {
-  res.status(200).sendFile(path.join(__dirname, "/views/userForms.html"));
+server.post(
+  "/userForms",
+  [
+    check("email").notEmpty(),
+    check("password").notEmpty(),
+  ],
+  (req, res) => {
+    var userLoginData = {
+      email: req.body.email,
+      password: req.body.password,
+    };
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      const errorMsgs = errors.array();
+      let emailError;
+      let passwordError;
 
-});
+      if (errorMsgs.some((el) => el.param === "email")) {
+        emailError = "Please enter your email address";
+      }
+      
+      if (errorMsgs.some((el) => el.param === "password")) {
+        passwordError = "Forgot your password ? ";
+      }
+      res.render("userForms", {
+        errorMsgs,
+        layout: "register",
+        data: userLoginData,
+        emailError,
+        passwordError
+      });
+    } else if (errors.isEmpty()) {
+      res.redirect(`/dashboard/${userLoginData.email}`);
+    }
+  }
+);
 
-
-app.get("/user-profile", (req , res) => {
-  res.status(200).sendFile(path.join(__dirname, "/views/user-profile.html"));
-});
+server.get("/dashboard/:email/:fName?/:lName?", (req, res) => {
+  let email = req.params.email;
+  let fName = req.params.fName;
+  let lName = req.params.lName;
+  res.render("dashboard", {
+    title: "Welcome",
+    layout: "userProfile",
+    email
+  })
+})
 
 //err handling
-app.use((req, res) => {
+server.use((req, res) => {
   res
     .status(404)
     .send(
       "<h3>The page you are looking for either misspelled or under construction :)</h3>"
     );
-}); 
+});
 
-app.listen(HTTP_PORT, onHttpStart);
+const HTTP_PORT = process.env.PORT || 8080;
+server.listen(HTTP_PORT, (err) => {
+  if (err)
+    console.log(err)
+  else
+    console.log(`=> Server started on port | ${HTTP_PORT} |`);
+});
