79948277dd6d91d1343c0ef98cb47bb5d3c9b189
Batuhan <bipci@myseneca.ca>
2021-11-11 15:50:03 +0300

    added favicon

--- scripts/displayDate.js
+++ scripts/displayDate.js
@@ -2,4 +2,4 @@ var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July',
 'August', 'September', 'October', 'November', 'December'];
 var tomorrow = new Date();
 tomorrow.setTime(tomorrow.getTime() + (1000 * 3600 * 24));
-document.getElementById("spanDate").innerHTML = months[tomorrow.getMonth()] + " " + tomorrow.getDate() + ", " + tomorrow.getFullYear() + "<br/><br/>" + "perfect day to order your Steak. Grilled just the way you like it!";
+document.getElementById("spanDate").innerHTML = months[tomorrow.getMonth()] + " " + tomorrow.getDate() + ", " + tomorrow.getFullYear() + "<br/>" + "perfect date to order your Steak. Grilled just the way you like it!" + "<br/><br/>";
