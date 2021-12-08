c567edb8498c56b3847ee45b5a1be0e23c10ecf5
Batuhan <bipci@myseneca.ca>
2021-12-08 03:34:23 +0300

    final bcrypt fix

--- routes/users.js
+++ routes/users.js
@@ -173,9 +173,9 @@ router.post('/login',
   (req, res) => {
     const {email,username,password} = req.body
     const errors = validationResult(req);
+    var loginErr = [];
 
     if (!errors.isEmpty()) {
-      let loginErr = [];
       User.findAll({
           where: {
             email: email,
@@ -210,10 +210,23 @@ router.post('/login',
               console.log(`User logged in with username: ${userlogin.email} 🚀`)
               req.session.User = userlogin;
               res.redirect('loginDashboard')
-            }
+            }else{
+              loginErr.push({
+                msg: "We cannot find an account with the provided credentials. Please create your account below"
+              })
+            }       
+            res.render("client/loginForm", {
+              title: "Sign in",
+              dataLogin: req.body,
+              loginErr,
+              email,
+              password,
+              layout: "forms"
+            });
+        
           }).catch(err => console.log(`🤡 Hashing error: ${err}`))
         }
-      }).catch(err => console.log(`something went wrong => ${err}`))
+      }).catch(err => console.log(`🤡something went wrong => ${err}`))
     }
   })
 
